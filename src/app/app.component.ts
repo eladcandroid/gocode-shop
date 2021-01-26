69268afbfc8b2250c23778f18aec827d24f3a067
eladcandroid <eladc.android@gmail.com>
2021-01-26 21:32:37 +0200

    Change title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Tour of Heroes :))';
+  title = 'Tour of Heroes 123';
   onSave() {
     console.log('onSave fired!');
   }
