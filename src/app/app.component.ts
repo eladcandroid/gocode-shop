3a27bd946edd6ef306677c0a04a8ecfc8a0fd078
eladcandroid <eladc.android@gmail.com>
2021-01-26 18:59:49 +0200

    Push to github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'gocode-shop';
+  title = 'Tour of Heroes';
+  onSave() {
+    console.log('onSave fired!');
+  }
+  onToggle() {
+    this.title = this.title ? '' : 'Tour of Heroes';
+  }
 }
